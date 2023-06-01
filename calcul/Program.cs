484386d04a4cc9774f266d451113ff9808fddb2b
Midzune <444ctvf56@mail.ru>
2023-06-01 17:38:05 +0300

    Диалог для выхода из программы

--- calcul/Program.cs
+++ calcul/Program.cs
@@ -19,6 +19,7 @@ namespace calcul
             oper = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("Введите второе число:");
             b = Convert.ToDouble(Console.ReadLine());
+            
             if (oper == '+')
             {
                 total = a + b;
@@ -38,6 +39,19 @@ namespace calcul
             {
                 total = a / b;
                 Console.WriteLine(a + " / " + b + " равно " + total + ".");
+            }
+
+            Console.WriteLine("Желаете ли выйти (Y/N)?");
+            oper = Convert.ToChar(Console.ReadLine());
+
+             if (oper == 'Y')
+            {
+                return;
+            }
+            else if (oper == 'N')
+            {
+                Console.WriteLine("Введите первое число:");
+                a = Convert.ToDouble(Console.ReadLine());
             }
         }
     }
